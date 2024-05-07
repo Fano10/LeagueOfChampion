df2be0496d7d845fb5239029b1bcc245b75cdcda
Rakotondrabe Miora Fanomezana <rsjasonfano@gmail.com>
2024-05-07 19:19:56 -0400

    add button (addPromise) and a dialog interface to insert a new promise

--- app/src/main/java/uqac/dim/leagueoflegend/MainActivity.java
+++ app/src/main/java/uqac/dim/leagueoflegend/MainActivity.java
@@ -1,8 +1,13 @@
 package uqac.dim.leagueoflegend;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addPromise(View v){
+        AlertDialog.Builder builder  = new AlertDialog.Builder(this); // pop up qu'on veut afficher pour ajouter des promesses
+        EditText editText = new EditText(this); // EditText qu'on va mettre dans le pop up
+        editText.setHint("nouvelle promesse");
+        builder.setTitle("Ajouter une nouvelle promesse");
+        builder.setView(editText);
+
+        builder.setPositiveButton("Ajouter", (dialog, which) -> {
+            Toast.makeText(this,editText.getText(),Toast.LENGTH_SHORT).show();
+            dialog.cancel();
+        });
+        builder.setNegativeButton("Annuler", (dialog, which) -> {
+            dialog.cancel();
+        });
+
+        builder.show();
     }
 }
